6cfce4620f76bbc5e8353f719b4261a290639a63
Hyp3rstatic <kazjrosinski@gmail.com>
2025-08-03 22:31:16 -0700

    solution pending...

--- sin/sin.py
+++ sin/sin.py
@@ -39,18 +39,17 @@ SEVEN_FACTORIAL = factorial(7) #5040
 #python math.pi approximation
 PI = 3.141592653589793
 
-#just giving vals in the range rn
 def sin(rad):
   #put radian value in terms of one unit circle cycle
   rad = rad % (PI*2)
 
   #put the radian value in terms of pi/2 (effective range)
-  
-  #PI/2 and 3PI/2 have to distinguished because of PI/2 being the clock
+  #PI/2 and 3PI/2 have to distinguished because of PI/2 being the clock base
   if rad == PI/2 or rad == (PI*3)/2:
-    rad_quart = rad
+    rad_quart = PI/2
   else:
-    rad_quart = rad % (PI/2)     
+    rad_quart = rad % (PI/2) 
+    #print("rad: " + str(rad)) #debug
 
   #handle flipping logic for rad_quart based on its quadrant
 
@@ -58,4 +57,29 @@ def sin(rad):
 
   return sin_val
 
-print(sin(PI/2))
+print("1: " + str(sin( PI/2 ))) #1
+print("0: " + str(sin( PI ))) #0
+print("-1: " + str(sin( (3*PI)/2 ))) #-1
+print("0: " + str(sin( 2*PI ))) #0
+
+print('')
+
+print("0.7: " + str(sin( PI/4 ))) #0.7
+print("0.7: " + str(sin( (3*PI)/4 ))) #0.7
+print("-0.7: " + str(sin( (5*PI)/4 ))) #-0.7
+print("-0.7: " + str(sin( (7*PI)/4 ))) #-0.7
+
+print('')
+
+print("0.86: " + str(sin( PI/3 ))) #0.86
+print("0.86: " + str(sin( (2*PI)/3 ))) #0.86
+print("-0.86:" + str(sin( (4*PI)/3 ))) #-0.86
+print("-0.86: " + str(sin( (5*PI)/3 ))) #-0.86
+
+print('')
+
+print("0.5: " + str(sin( PI/6 ) )) #0.5
+print("0.5: " + str(sin( (5*PI)/6 ))) #0.5
+print("-0.5: " + str(sin( (7*PI)/6 ))) #-0.5
+print("-0.5: " + str(sin( (11*PI)/6 ))) #-0.5
+
